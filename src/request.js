a64826135ee7cc9e9fb6adaa211e8c07aad1941b
Ajiharan <53114753+Ajiharan@users.noreply.github.com>
2020-08-02 00:07:31 +0530

    slide_changes

--- src/request.js
+++ src/request.js
@@ -1,15 +1,14 @@
-const API_KEY="9457a2cd8806c9ce2f824317fc07fec9";
+const API_KEY = "";
 
-const request={
-    fetchTrending:`/trending/all/week?api_key=${API_KEY}&language=en-us`,
-    fetchNetflixOriginals:`/discover/tv?api_key=${API_KEY}&with_network=213`,
-    fetchTopRated:`/movie/top_rated?api_key=${API_KEY}&language=en-US`,
-    fetchActionMovies:`/discover/movie?api_key=${API_KEY}&with_genres=28`,
-    fetchCommedyMovies:`/discover/movie?api_key=${API_KEY}&with_genres=35`,
-    fetchHorrorMovies:`/discover/movie?api_key=${API_KEY}&with_genres=27`,
-    fetchRomanceMovies:`/discover/movie?api_key=${API_KEY}&with_genres=10749`,
-    fetchDOcumentaries:`/discover/movie?api_key=${API_KEY}&with_genres=99`
-}
-
+const request = {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-us`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_network=213`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchCommedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDOcumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+};
 
 export default request;
